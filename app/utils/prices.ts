a33d463a024f2cd01a45a1238ec7f80d87e128eb
Leonel Donati <leonelroman321@gmail.com>
2025-04-04 19:49:08 -0300

    fixed ts errors

--- app/utils/prices.ts
+++ app/utils/prices.ts
@@ -1,8 +1,1 @@
-export function calculateFinalPrice(products: Product[]) {
-  let sum = 0;
-  products.forEach((prod) => {
-    prod.presentations.forEach((pres) => (sum += pres.price * pres.quantity));
-  });
 
-  return sum;
-}
